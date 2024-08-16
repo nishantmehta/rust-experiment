187829176619c612cb309521415aee2a16980037
Nishant Mehta <nishantmehta@users.noreply.github.com>
2024-08-16 18:04:38 +0000

    Attempt to add logging to the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use log::{info, trace, warn};
+
 fn main(){
+    log::trace!("Commencing yak shaving");
+    
+    //log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     println!("Hello world!");
+    log::warn!("Hello world!");
 }
