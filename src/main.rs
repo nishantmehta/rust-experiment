13f726e65871d4db6ee7a712d474128fe78002d4
Nishant Mehta <nishantmehta@users.noreply.github.com>
2024-08-16 18:19:25 +0000

    Logging now works!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use log::{info, trace, warn};
+use log::info;
+use log4rs;
 
 fn main(){
-    log::trace!("Commencing yak shaving");
-    
-    //log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    info!("Commencing yak shaving");
     println!("Hello world!");
-    log::warn!("Hello world!");
+    info!("Hello world!");
 }
